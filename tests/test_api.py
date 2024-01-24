171030482ec7c34b9a35f24df8d920dbc3315b86
Andrii Zaluzhnyi <andrii.zaluzhnyi@cloverpop.com>
2024-01-24 11:50:15 +0200

    update test case

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,11 @@
 # import unittest
+import sys
+import os
+
+sys.path.append(os.path.abspath('.'))
 
 from app import template_hello
+
 
 def test_hello_world():
     assert template_hello() == "<p>Hello, World!</p>"
