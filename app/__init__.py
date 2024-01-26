1e57cd1941ca70d2caae2fd1d359ef5a6d7193f2
Andrii Zaluzhnyi <andrii.zaluzhnyi@cloverpop.com>
2024-01-26 10:51:25 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import requests
+import json
 
 def template_hello():
     return "<p>Hello, World!</p>"
@@ -7,3 +9,17 @@ def divide(num1, num2):
     if num2 == 0:
         raise ZeroDivisionError
     return num1 / num2
+
+
+
+
+# def hello_world():
+#     response = requests.get(url, headers=header)
+#     data = json.loads(response.text)
+#     print(response.status_code)
+#     return response.status_code
+
+
+
+if __name__ == '__main__':
+    hello_world()
