635c57a2102aadcb257cf6000618c81cd047d2fa
andrii.zaluzhnyi <andrii.zaluzhnyi@cloverpop.com>
2024-07-04 11:01:51 +0300

    test

--- app/__init__.py
+++ app/__init__.py
@@ -21,16 +21,3 @@ def divide(num1, num2):
         raise ZeroDivisionError
     return num1 / num2
 
-
-
-
-# def hello_world():
-#     response = requests.get(url, headers=header)
-#     data = json.loads(response.text)
-#     print(response.status_code)
-#     return response.status_code
-
-
-
-# if __name__ == '__main__':
-#     hello_world()
